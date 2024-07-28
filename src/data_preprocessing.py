d476345342d1cb27222f192911f282ce41b639a1
DeeDeeShiba <a.davidromero@icloud.com>
2024-07-28 01:06:08 -0400

    Initial Commit - Created project skeleton and conda python 3.9 env

---
+++ src/data_preprocessing.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import torch
+import librosa
+import jupyter
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("All packages imported successfully!")
